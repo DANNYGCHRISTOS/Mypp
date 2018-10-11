8c16e9cd0bd10ca1a7ca4f2beb505820d6d82103
daniel <seahagos@gmail.com>
2018-10-10 23:27:16 -0700

    home

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+class Home extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="headline">
+        <div className="body">
+          <div className="leftbox">lb</div>
+          <div className="rightbox">rb</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
